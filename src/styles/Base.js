d1d4278a170fa39d8851f45ca889730912778c31
penny <penny@penny.lgbt>
2017-02-17 14:53:44 +0100

    make containers responsive

--- src/styles/Base.js
+++ src/styles/Base.js
@@ -3,11 +3,15 @@ import styled from 'styled-components'
 export const Container = styled.div`
   font-family: sans-serif;
   margin: 20px 20vw;
+
+  @media (max-width: 420px) {
+    margin: 20px 5vw;
+  }
 `
 
 export const Link = styled.a`
   text-decoration: none;
-  color: darkslateblue;
+  color: #3F51B5;
 `
 
 export const BorderLink = styled(Link)`
