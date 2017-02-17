3b2d0d0af1d8c2d21f88dac1f01cdeff4b9bf1a2
penny <penny@penny.lgbt>
2017-02-17 13:03:36 +0100

    use styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 import RepoList from './components/RepoList'
+
+const Container = styled.div`
+  font-family: sans-serif;
+  margin: 20px 20vw;
+`
+
+const Heading = styled.h1`
+  font-size: 24px;
+`
+
+const Small = styled.span`
+  font-size: 16px;
+  font-weight: normal;
+`
 
 const App = () => {
   return (
-    <section>
-      <h1>duet</h1>
+    <Container>
+      <Heading>duet <Small>accessible issue browser for jekyll</Small></Heading>
       <RepoList />
-    </section>
+    </Container>
   )
 }
 
