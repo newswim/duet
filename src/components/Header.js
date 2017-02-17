6b71654a7abca99f39024a8f679b6dc49c6876a4
penny <penny@penny.lgbt>
2017-02-17 15:06:44 +0100

    add description

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Container, HeaderContainer, BigHeading } from '../styles'
+import {
+  Container,
+  HeaderContainer,
+  BigHeading,
+  HeaderDescription
+} from '../styles'
 
 const Header = () => {
   return (
     <HeaderContainer>
       <Container>
         <BigHeading>Welcome to duet!</BigHeading>
-        <p>
+        <HeaderDescription>
           duet is a tool that helps newcomers to open-source find
-          their first contribution to Jekyll.
-        </p>
+          their first contribution to Jekyll, and to help maintainers
+          keep an overview.
+        </HeaderDescription>
       </Container>
     </HeaderContainer>
   )
--- src/styles/Header.js
+++ src/styles/Header.js
@@ -4,3 +4,13 @@ export const HeaderContainer = styled.div`
   background-color: whitesmoke;
   padding: 10px 20px;
 `
+
+export const HeaderDescription = styled.p`
+  width: 70%;
+  line-height: 27px;
+  font-size: 18px;
+
+  @media (max-width: 420px) {
+    width: 100%;
+  }
+`
