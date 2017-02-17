84ff36e5245b5120324a1211671157b005886ccb
penny <penny@penny.lgbt>
2017-02-17 11:21:24 +0100

    implement fetching from github

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Issue from './Issue'
 
-const Repo = props => {
-  return (
-    <li>
-      <h2>{props.name}</h2>
-      <p>{props.description}</p>
-      {console.log(props.issues)}
-      {props.issues.length === 0 ? <p>No issues currently!</p> : null}
-      {props.issues.map((issue, i) => {
-        return <Issue key={i} {...issue} />
-      })}
-    </li>
-  )
+class Repo extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      details: {},
+      issues: []
+    }
+  }
+
+  componentWillMount () {
+    Promise.all([
+      this.props.repo.getDetails(),
+      this.props.issues.listIssues({ labels: 'first-timers-only', state: 'all' })
+    ]).then(values => {
+      this.setState({
+        details: values[0].data,
+        issues: values[1].data
+      })
+    })
+  }
+
+  render () {
+    return (
+      <li>
+
+        <h2>{this.state.details.name}</h2>
+        <p>stars: {this.state.details.stargazers_count}</p>
+        {this.state.issues.length === 0 ? <p>No issues currently!</p> : null}
+        {this.state.issues.map((issue, i) => {
+          return <Issue key={i} {...issue} />
+        })}
+      </li>
+    )
+  }
 }
 
 export default Repo
--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import GitHub from 'github-api'
 import Repo from './Repo'
 
 class RepoList extends Component {
@@ -8,22 +9,20 @@ class RepoList extends Component {
   }
 
   componentWillMount () {
-    this.setState({repos: [
-      {
-        'name': 'jekyll',
-        'description': 'blah',
-        'issues': [
-          { 'id': 1, 'label': 'first-timers-only', 'title': 'im cool' }
-        ]
-      }
-    ]})
+    const gh = new GitHub()
+    const repos = ['jekyll/jekyll']
+    this.setState({
+      repos: repos.map(repo => gh.getRepo(...repo.split('/'))),
+      issues: repos.map(repo => gh.getIssues(...repo.split('/')))
+    })
+    console.log(this.state.repos)
   }
 
   render () {
     return (
       <ul>
         {this.state.repos.map((repo, i) => {
-          return <Repo key={i} {...repo} />
+          return <Repo key={i} repo={repo} issues={this.state.issues[i]} />
         })}
       </ul>
     )
